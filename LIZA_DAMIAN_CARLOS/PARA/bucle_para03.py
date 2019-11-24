5246c7d52014c8468f6221d339509593cb743cc7
CARLOSC10 <56972451+carlosc10@users.noreply.github.com>
2019-11-24 15:04:13 -0500

    se agrego el 3er ejercicio de bucle para

---
+++ LIZA_DAMIAN_CARLOS/PARA/bucle_para03.py
@@ -0,0 +1,15 @@
+#REPETITIVAS "PARA" QUE MUESTRA CUALQUIER TEXTO ESCRITO POR TECLADO VECES 20 VECES
+import os
+palabras=""
+
+#ARGUMENTOS
+palabras=os.sys.argv[1]
+
+#PROCESSING DE LA ESTRUCTURA "PARA"
+i=0
+while(i<20):
+    print(palabras)
+    i+=1
+#fin_while
+
+
