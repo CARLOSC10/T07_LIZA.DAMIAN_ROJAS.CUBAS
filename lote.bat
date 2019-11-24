cdca7615a325d0a180b49db553e484ff890e0b69
CARLOSC10 <56972451+carlosc10@users.noreply.github.com>
2019-11-24 15:28:52 -0500

    se agrego el lote.bat

---
+++ lote.bat
@@ -0,0 +1,25 @@
+python LIZA_DAMIAN_CARLOS/MIENTRAS/bucle_mientras01.py "Carlos" 42 10 15
+python LIZA_DAMIAN_CARLOS/MIENTRAS/bucle_mientras02.py 50 10 150
+python LIZA_DAMIAN_CARLOS/MIENTRAS/bucle_mientras03.py 20 15 18 20
+python LIZA_DAMIAN_CARLOS/MIENTRAS/bucle_mientras04.py 52 200 13
+python LIZA_DAMIAN_CARLOS/MIENTRAS/bucle_mientras05.py 88 23 154
+
+python LIZA_DAMIAN_CARLOS/PARA/bucle_para01.py 15
+python LIZA_DAMIAN_CARLOS\PARA\bucle_para02.py 28
+python LIZA_DAMIAN_CARLOS\PARA\bucle_para03.py "HOLA MUNDO"
+python LIZA_DAMIAN_CARLOS\PARA\bucle_para04.py 19
+python LIZA_DAMIAN_CARLOS\PARA\bucle_para05.py "PROGRAMACION"
+
+python LIZA_DAMIAN_CARLOS\ITERACION\bucle_iteracion01.py "FERNANDO"
+python LIZA_DAMIAN_CARLOS\ITERACION\bucle_iteracion02.py "ESTEFANY"
+python LIZA_DAMIAN_CARLOS\ITERACION\bucle_iteracion03.py 8
+python LIZA_DAMIAN_CARLOS\ITERACION\bucle_iteracion04.py "BUCLES"
+python LIZA_DAMIAN_CARLOS\ITERACION\bucle_iteracion05.py "REPETITIVAS"
+
+
+python LIZA_DAMIAN_CARLOS\ITERACION_RANGO\bucle_iteracion_rango01.py 14
+python LIZA_DAMIAN_CARLOS\ITERACION_RANGO\bucle_iteracion_rango02.py 8
+python LIZA_DAMIAN_CARLOS\ITERACION_RANGO\bucle_iteracion_rango03.py "MATEMATICA II"
+python LIZA_DAMIAN_CARLOS\ITERACION_RANGO\bucle_iteracion_rango04.py 4
+python LIZA_DAMIAN_CARLOS\ITERACION_RANGO\bucle_iteracion_rango05.py 10
+
