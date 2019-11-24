5dc51fbe321804cb70a0dc377ebdc242d8ddfb3e
CARLOSC10 <56972451+carlosc10@users.noreply.github.com>
2019-11-24 15:04:52 -0500

    se agrego el 5to ejercicio de bucle para

---
+++ LIZA_DAMIAN_CARLOS/PARA/bucle_para05.py
@@ -0,0 +1,16 @@
+#REPETITIVAS "PARA" QUE MUESTRA CUALQUIER TEXTO ESCRITO POR TECLADO VECES 100 VECES
+import os
+palabras=""
+
+#ARGUMENTOS
+palabras=os.sys.argv[1]
+
+#PROCESSING DE LA ESTRUCTURA "PARA"
+i=0
+while(i<100):
+    print(palabras)
+    i+=1
+#fin_while
+
+
+
