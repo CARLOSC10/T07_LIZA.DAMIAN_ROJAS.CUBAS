1ce8d2f6dc306d6b291a6617f8a9f45b9027f775
CARLOSC10 <56972451+CARLOSC10@users.noreply.github.com>
2019-11-24 15:48:43 -0500

    Update lote.bat

--- lote.bat
+++ lote.bat
@@ -23,3 +23,24 @@ python LIZA_DAMIAN_CARLOS\ITERACION_RANGO\bucle_iteracion_rango03.py "MATEMATICA
 python LIZA_DAMIAN_CARLOS\ITERACION_RANGO\bucle_iteracion_rango04.py 4
 python LIZA_DAMIAN_CARLOS\ITERACION_RANGO\bucle_iteracion_rango05.py 10
 
+
+
+
+
+python rojascubas/mientras/ejercicio1.py 10 20 10 07
+python rojascubas/mientras/ejercicio2.py juan 12345F 1234567
+python rojascubas/mientras/ejercicio3.py 14 15 16
+python rojascubas/mientras/ejercicio4.py 5 10 2
+python rojascubas/mientras/ejercicio5.py miercoles 15 julio 2016
+python rojascubas/para/ejercicio6.py 22
+python rojascubas/para/ejercicio7.py  21
+python rojascubas/para/ejercicio8.py  15
+python rojascubas/para/ejercicio9.py  14
+python rojascubas/para/ejercicio10.py  -10 10
+python rojascubas/iteracion/ejercicio11.py hola bienbenido
+python rojascubas/iteracion/ejercicio12.py 94547214
+python rojascubas/iteracion/ejercicio13.py 14521
+python rojascubas/iteracion/ejercicio14.py  hola.comoestas....
+python rojascubas/iteracion/ejercicio15.py  progra12345ciclo
+python rojascubas/iteracionenrango/ejercicio16.py 5
+python rojascubas/iteracionenrango/ejercicio17.py -5 5
