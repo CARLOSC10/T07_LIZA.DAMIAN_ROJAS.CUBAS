035afeb90429374694b9d1a197ec705db12979ae
CARLOSC10 <56972451+CARLOSC10@users.noreply.github.com>
2019-11-24 15:49:35 -0500

    Update lote.err.bat

--- lote.err.bat
+++ lote.err.bat
@@ -21,3 +21,29 @@ python LIZA_DAMIAN_CARLOS\ITERACION_RANGO\bucle_iteracion_rango02.py a
 python LIZA_DAMIAN_CARLOS\ITERACION_RANGO\bucle_iteracion_rango03.py &&
 python LIZA_DAMIAN_CARLOS\ITERACION_RANGO\bucle_iteracion_rango04.py D
 python LIZA_DAMIAN_CARLOS\ITERACION_RANGO\bucle_iteracion_rango05.py 8P
+
+
+
+
+
+
+
+
+python rojascubas/mientras/ejercicio1.py -10 20 10 07
+python rojascubas/mientras/ejercicio2.py juana 1555 ebchebc
+python rojascubas/mientras/ejercicio3.py -5 -2 4
+python rojascubas/mientras/ejercicio4.py -10 2 -4
+python rojascubas/mientras/ejercicio5.py juerves 31 junnio 1200
+python rojascubas/para/ejercicio6.py -7
+python rojascubas/para/ejercicio7.py 18
+python rojascubas/para/ejercicio8.py -2
+python rojascubas/para/ejercicio9.py -20
+python rojascubas/para/ejercicio10.py 0 0
+python rojascubas/iteracion/ejercicio11.py 12345
+python rojascubas/iteracion/ejercicio12.py 423456789
+python rojascubas/iteracion/ejercicio13.py -4651
+python rojascubas/iteracion/ejercicio14.py 15112hola
+python rojascubas/iteracion/ejercicio15.py ""
+python rojascubas/iteracionenrango/ejercicio16.py -5
+python rojascubas/iteracionenrango/ejercicio17.py -13 10
+
