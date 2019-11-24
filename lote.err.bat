1384b71f1e2c7f8cb007c4c46c5e7ec526db7ae6
CARLOSC10 <56972451+carlosc10@users.noreply.github.com>
2019-11-24 15:28:26 -0500

    se agrego el lote.err.bat

---
+++ lote.err.bat
@@ -0,0 +1,23 @@
+python LIZA_DAMIAN_CARLOS/MIENTRAS/bucle_mientras01.py "Frank" 25 10 15
+python LIZA_DAMIAN_CARLOS/MIENTRAS/bucle_mientras02.py -10 -8 50
+python LIZA_DAMIAN_CARLOS/MIENTRAS/bucle_mientras03.py -8 15 -1 20
+python LIZA_DAMIAN_CARLOS/MIENTRAS/bucle_mientras04.py -21 17 4
+python LIZA_DAMIAN_CARLOS/MIENTRAS/bucle_mientras05.py 97 3 38
+
+python LIZA_DAMIAN_CARLOS/PARA/bucle_para01.py -25
+python LIZA_DAMIAN_CARLOS\PARA\bucle_para02.py -21
+python LIZA_DAMIAN_CARLOS\PARA\bucle_para03.py &&&
+python LIZA_DAMIAN_CARLOS\PARA\bucle_para04.py -8
+python LIZA_DAMIAN_CARLOS\PARA\bucle_para05.py &&
+
+python LIZA_DAMIAN_CARLOS\ITERACION\bucle_iteracion01.py &&
+python LIZA_DAMIAN_CARLOS\ITERACION\bucle_iteracion02.py &&&
+python LIZA_DAMIAN_CARLOS\ITERACION\bucle_iteracion03.py OE
+python LIZA_DAMIAN_CARLOS\ITERACION\bucle_iteracion04.py &
+python LIZA_DAMIAN_CARLOS\ITERACION\bucle_iteracion05.py &
+
+python LIZA_DAMIAN_CARLOS\ITERACION_RANGO\bucle_iteracion_rango01.py -15
+python LIZA_DAMIAN_CARLOS\ITERACION_RANGO\bucle_iteracion_rango02.py a
+python LIZA_DAMIAN_CARLOS\ITERACION_RANGO\bucle_iteracion_rango03.py &&
+python LIZA_DAMIAN_CARLOS\ITERACION_RANGO\bucle_iteracion_rango04.py D
+python LIZA_DAMIAN_CARLOS\ITERACION_RANGO\bucle_iteracion_rango05.py 8P
