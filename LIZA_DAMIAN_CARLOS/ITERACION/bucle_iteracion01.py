3216964486f35ec15e324a46d42ed9918128499f
CARLOSC10 <56972451+carlosc10@users.noreply.github.com>
2019-11-24 15:05:21 -0500

    se agrego el 1er ejercicio de bucle iteracion

---
+++ LIZA_DAMIAN_CARLOS/ITERACION/bucle_iteracion01.py
@@ -0,0 +1,13 @@
+#REPETITIVAS ITERACION QUE INPRIME CUALQUIER CADENA ESCRITA POR TECLADO
+import os
+nombre=""
+
+#ARGUMENTO
+#ASIGNACION DE VALORES
+nombre=os.sys.argv[1]
+
+#PROCESSING DE LA ESTRUCTURA "ITERACION"
+for x in nombre:
+    print(x)
+#fin_iterar
+print("FIN DEL BUCLE")
